514f78d40408657e653e2f4c5557cfe1ccfd3347
Juliano Padilha <juliano.padilha@mercadolivre.com>
2021-02-04 15:32:18 -0300

    feat: mapping routers on controller

--- src/application/entrypoints/api/actionItem/controller.ts
+++ src/application/entrypoints/api/actionItem/controller.ts
@@ -3,6 +3,11 @@ import { ActionItem } from '../../../../domain/entities/ActionItem';
 
 /* Usecase */
 import createActionItem from '../../../../domain/usecases/createActionItem';
+import getAllActionItems from '../../../../domain/usecases/getAllActionItems';
+import getActionItemById from '../../../../domain/usecases/getActionItemById';
+import searchActionItemsByTitleContent from '../../../../domain/usecases/searchActionItemsByTitleContent';
+import updateActionItem from '../../../../domain/usecases/updateActionItem';
+import removeActionItemById from '../../../../domain/usecases/removeActionItemById';
 
 /* Gateway Interface */
 import ActionItemInterface from '../../../gateways/ActionItemInterface';
@@ -13,7 +18,65 @@ class ActionItemController {
       const result = await createActionItem(ActionItemInterface, req.body);
       return res.json(result);
     } catch (err) {
-      console.log('EEEE', err);
+      console.log('createActionItem -> error: ', err);
+    }
+  }
+
+  async getAllActionItems(req: any, res: any): Promise<ActionItem> {
+    try {
+      const result = await getAllActionItems(ActionItemInterface);
+      return res.json(result);
+    } catch (err) {
+      console.log('getAllActionItems -> error: ', err);
+    }
+  }
+
+  async getActionItemById(req: any, res: any): Promise<ActionItem> {
+    const {
+      itemId
+    } = req.params;
+    try {
+      const result = await getActionItemById(ActionItemInterface, itemId);
+      return res.json(result);
+    } catch (err) {
+      console.log('getActionItemById -> err', err);
+    }
+  }
+
+  async searchActionItemsByTitleContent(req: any, res: any): Promise<ActionItem> {
+    const {
+      search
+    } = req.query;
+    try {
+      const result = await searchActionItemsByTitleContent(ActionItemInterface, search);
+      return res.json(result);
+    } catch (err) {
+      console.log('searchActionItemsByTitleContent -> err', err);
+    }
+  }
+
+  async updateActionItem(req: any, res: any): Promise<ActionItem> {
+    const {
+      params: { itemId },
+      body
+    } = req
+    try {
+      const result = await updateActionItem(ActionItemInterface, itemId, body);
+      return res.json(result);
+    } catch (err) {
+      console.log('updateActionItem -> err', err);
+    }
+  }
+
+  async removeActionItemById(req: any, res: any): Promise<ActionItem> {
+    const {
+      itemId
+    } = req.params;
+    try {
+      const result = await removeActionItemById(ActionItemInterface, itemId);
+      return res.json(result);
+    } catch (err) {
+      console.log('removeActionItemById -> err', err);
     }
   }
 }
--- src/application/entrypoints/api/actionItem/index.ts
+++ src/application/entrypoints/api/actionItem/index.ts
@@ -7,5 +7,9 @@ import ActionItemController from './controller';
 const router: IRouter = express.Router();
 
 router.post('/', ActionItemController.createActionItem);
+router.put('/:itemId', ActionItemController.updateActionItem);
+router.get('/', ActionItemController.getAllActionItems);
+router.get('/:itemId', ActionItemController.getActionItemById);
+router.delete('/:itemId', ActionItemController.removeActionItemById);
 
 export default router;
