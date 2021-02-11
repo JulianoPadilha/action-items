8fd65f7135b3fe15eb85085c1bb2f49aafe1968b
Juliano Padilha <juliano.padilha@mercadolivre.com>
2021-02-11 14:50:28 -0300

    feat: add makeFetch utils

---
+++ src/application/entrypoints/app/utils/makeFetch.ts
@@ -0,0 +1,52 @@
+/* Entities */
+import { ActionItem } from '../../../../domain/entities/ActionItem';
+
+class makeFetch {
+  async get(url: string): Promise<ActionItem> {
+    const response = await fetch(url, {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    return response.json();
+  }
+
+  async post(url: string, data: ActionItem): Promise<ActionItem> {
+    const response = await fetch(url, {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    });
+    return response.json();
+  }
+
+  async put(url: string, data: ActionItem): Promise<ActionItem> {
+    const response = await fetch(url, {
+      method: 'PUT',
+      mode: 'cors',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    });
+    return response.json();
+  }
+
+  async delete(url: string): Promise<ActionItem> {
+    const response = await fetch(url, {
+      method: 'DELETE',
+      mode: 'cors',
+      headers: {
+        "Content-Type": "application/json"
+      },
+    });
+    return response.json();
+  }
+}
+
+export default makeFetch;
