f07172ad5a8dbe439c63042ea9b3378847050847
Juliano Padilha <juliano.padilha@mercadolivre.com>
2021-02-11 14:53:28 -0300

    feat: update ActionItem test

---
+++ src/domain/entities/__tests__/ActionItem.spec.ts
@@ -0,0 +1,38 @@
+/* Entity */
+import { ActionItem } from '../ActionItem';
+import { TypeEnum } from '../enums/ActionItemType'
+import { StatusEnum } from '../enums/ActionItemStatus'
+
+const actionItemTypeMock: ActionItem = {
+  title: 'Title',
+  description: 'Description',
+  sprintDate: new Date("2021-02-04 12:00:00 GMT-0"),
+  sprintName: 'Sprint name',
+  status: StatusEnum.DONE,
+  type: TypeEnum.EXECUTABLE,
+  owner: 'Joe Doe',
+}
+
+describe('ActionItem Entity', () => {
+  it('should entity have all the properties', () => {
+    Object.keys(actionItemTypeMock).forEach(key => {
+      expect(actionItemTypeMock).toHaveProperty(key);
+    });
+  });
+
+  it('should entity have at least one property missing', () => {
+    //@ts-ignore
+    const actionItemTypeMockFail: ActionItem = {
+      description: 'Description',
+      sprintDate: new Date("2021-02-04 12:00:00 GMT-0"),
+      sprintName: 'Sprint name',
+      status: StatusEnum.DONE,
+      type: TypeEnum.EXECUTABLE,
+      owner: 'Joe Doe',
+    };
+
+    Object.keys(actionItemTypeMockFail).forEach(() => {
+      expect (actionItemTypeMockFail).not.toHaveProperty('title');
+    });
+  });
+});
