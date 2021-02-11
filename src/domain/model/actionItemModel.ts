8c117327ccb642feb1bc6dcb8a20e35b92c0754c
Juliano Padilha <juliano.padilha@mercadolivre.com>
2021-02-11 14:53:13 -0300

    feat: update ActionItem model

--- src/domain/model/actionItemModel.ts
+++ src/domain/model/actionItemModel.ts
@@ -4,11 +4,14 @@ import mongoose from 'mongoose';
 const actionItemSchema = new mongoose.Schema({
   title: String,
   description: String,
-  sprintDate: String,
-  sprintName: String,
+  creationDate: String,
+  sprint: String,
   status: String,
   type: String,
-  owner: String
+  owner: String,
+  priority: String,
+  assignmentDay: String,
+  endDay: String
 });
 
 export default mongoose.model('actionItem', actionItemSchema);
