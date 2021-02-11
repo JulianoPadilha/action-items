4a6eb29a7211f29b8fbca134871f90df7d165e56
Juliano Padilha <juliano.padilha@mercadolivre.com>
2021-02-11 14:42:28 -0300

    feat: add ContentNotFound component

---
+++ src/application/entrypoints/app/components/ContentNotFound/ContentNotFound.tsx
@@ -0,0 +1,12 @@
+/* Dependencies */
+import React from 'react';
+
+const ContentNotFound = () => {
+  return (
+    <div className="block ml-auto mr-auto mt-10 text-base text-gray-500">
+      <p>Não encontramos nada no momento. =/</p>
+    </div>
+  )
+}
+
+export default ContentNotFound;
