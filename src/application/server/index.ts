f108adf8fb0a28e67438bec02835f45b85997ca6
Juliano Padilha <juliano.padilha@mercadolivre.com>
2021-02-13 20:41:54 -0300

    chore: try to fiz deploy problem

--- src/application/server/index.ts
+++ src/application/server/index.ts
@@ -1,2 +1,36 @@
-export * as apiRouter from './apiRouter';
-export * as appRouter from './appRouter';
+// export * as apiRouter from './apiRouter';
+// export * as appRouter from './appRouter';
+
+/* Dependencies */
+import express from 'express';
+import path from 'path';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+/* Database */
+import connectDB from '../../database/db';
+
+/* Routers */
+import actionItemRouter from '../entrypoints/api/actionItem';
+
+const router = express();
+const PORT = 8000;
+
+connectDB();
+
+router.listen(process.env.PORT || PORT, () => console.log(`Server is listening on port ${PORT}`));
+
+router.use(cors());
+router.use((bodyParser.json()));
+router.use('/api/action-item', actionItemRouter);
+
+if (process.env.NODE_ENV === 'production') {
+  console.log('process.env.NODE_ENV', process.env.NODE_ENV);
+  router.use(express.static(path.resolve('./build')));
+  
+  router.get('/', (req: any, res: any) => {
+    res.sendFile(path.resolve('./build', 'index.html'));
+  });
+}
+
+export default router;
