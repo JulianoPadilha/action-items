f206a529f760f77a38fb4e8cf5ed6c26c60fc965
Juliano Padilha <juliano.padilha@mercadolivre.com>
2021-02-15 15:30:56 -0300

    refactor: add index.d.ts exposing a module for .png files

--- src/application/entrypoints/app/components/Header/Header.tsx
+++ src/application/entrypoints/app/components/Header/Header.tsx
@@ -2,14 +2,13 @@
 import React from 'react';
 
 /* Logo */
-/* @ts-ignore */
 import logo from '../../assets/images/logo.png';
 
 const Header = () => {
   return (
     <div className="flex justify-center">
       <a href="/" title="Action Items">
-        <img className="w-60 ml-auto mr-auto pt-6 pb-3" src={logo} alt="Action Items" />
+        <img className="w-60 ml-auto mr-auto pt-6 pb-3" src={logo} alt="SI-Accounts - Action Items" />
       </a>
     </div>
   )
