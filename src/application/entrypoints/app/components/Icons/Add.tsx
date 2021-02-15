41a3f2999763bcdcce5a2d15af6a82fc32371d1a
Juliano Padilha <juliano.padilha@mercadolivre.com>
2021-02-15 15:46:33 -0300

    refactor: add types for all icons

--- src/application/entrypoints/app/components/Icons/Add.tsx
+++ src/application/entrypoints/app/components/Icons/Add.tsx
@@ -1,7 +1,10 @@
 /* Dependencies */
-import React from 'react';
+import React, { FunctionComponent } from 'react';
 
-const Add = ({ className }: any) => {
+/* Types */
+import { IconProps } from './types';
+
+const Add: FunctionComponent<IconProps> = ({ className }) => {
   return (
     <svg className={className} width="30" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
