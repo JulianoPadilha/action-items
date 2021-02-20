847aa81c46505acf2253068796d1c054369b4290
Juliano Padilha <juliano.padilha@mercadolivre.com>
2021-02-20 19:41:37 -0300

    feat: ensure star icon will show when action item is on sprint

--- src/application/entrypoints/app/components/Row/Row.tsx
+++ src/application/entrypoints/app/components/Row/Row.tsx
@@ -21,6 +21,8 @@ import ContentNotFound from '../ContentNotFound/ContentNotFound';
 
 /* Icons */
 import Spinner from '../Icons/Spinner';
+import Star from '../Icons/Star';
+
 
 const Row: FunctionComponent<RowTypes> = ({
   items,
@@ -31,39 +33,41 @@ const Row: FunctionComponent<RowTypes> = ({
   return (
     <div className="flex flex-wrap">
       <Header />
-      {!isLoading && items.length ? items.map(item => (
-        <div key={item._id} className="flex w-full m-4">
-          <ul className="grid grid-cols-9 w-full py-3 text-center items-center text-base text-gray-500">
-            <li>
-              <Title openModalDetail={() => openModalDetail(item._id)} title={item.title} />
-            </li>
-            <li className={`mx-auto priority-${item.priority.toLocaleLowerCase()}`}>
-              <Priority priority={item.priority} />
-            </li>
-            <li>
-              <AssignmentDay assignmentDay={item.assignmentDay} />
-            </li>
-            <li>
-              <EndDay endDay={item.endDay} />
-            </li>
-            <li>
-              <Status status={item.status} />
-            </li>
-            <li>
-              <Type type={item.type} />
-            </li>
-            <li>
-              <Owner owner={item.owner} />
-            </li>
-            <li className="mx-auto text-green-700 gap-10">
-              <Edit itemId={item._id} />
-            </li>
-            <li className="mx-auto cursor-pointer text-red-500">
-              <Delete openModalDeleteActionItem={() => openModalDeleteActionItem(item)} />
-            </li>
-          </ul>
-        </div>
-      )) : !isLoading && !items.length ? <ContentNotFound />  : <Spinner />
+      {!isLoading && items.length ? items.map(item => {
+        return (
+          <div key={item._id} className="flex w-full m-4">
+            <ul className="grid grid-cols-9 w-full py-3 text-center items-center text-base text-gray-500">
+              <li>
+                { item.sprint && <Star className="mx-auto mb-2" /> }
+                <Title openModalDetail={() => openModalDetail(item._id)} title={item.title} />
+              </li>
+              <li className={`mx-auto priority-${item.priority.toLocaleLowerCase()}`}>
+                <Priority priority={item.priority} />
+              </li>
+              <li>
+                <AssignmentDay assignmentDay={item.assignmentDay} />
+              </li>
+              <li>
+                <EndDay endDay={item.endDay} />
+              </li>
+              <li>
+                <Status status={item.status} />
+              </li>
+              <li>
+                <Type type={item.type} />
+              </li>
+              <li>
+                <Owner owner={item.owner} />
+              </li>
+              <li className="mx-auto text-green-700 gap-10">
+                <Edit itemId={item._id} />
+              </li>
+              <li className="mx-auto cursor-pointer text-red-500">
+                <Delete openModalDeleteActionItem={() => openModalDeleteActionItem(item)} />
+              </li>
+            </ul>
+          </div>
+        ) }) : !isLoading && !items.length ? <ContentNotFound />  : <Spinner />
       }
     </div>
   )
