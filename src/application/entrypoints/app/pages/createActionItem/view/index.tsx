4915341c0c7fedc7c0336f6076e096ef8be488ac
Juliano Padilha <juliano.padilha@mercadolivre.com>
2021-02-20 18:48:59 -0300

    refactor: change creation and edition of action items to allow add more than one owner

--- src/application/entrypoints/app/pages/createActionItem/view/index.tsx
+++ src/application/entrypoints/app/pages/createActionItem/view/index.tsx
@@ -28,17 +28,29 @@ const CreateActionItem = () => {
   }
 
   const onFieldChange = (event: any) => {
-    const fieldName = event.target.id;
-    const fieldValue = event.target.value;
-    if (fieldName && fieldValue) {
-      //Copy previously state value and aggregate new values
-      setFields((state: any) => ({ ...state, [fieldName]: fieldValue }));
+    //@ts-ignore
+    const form = document.forms.actionItem;
+    const { title, description, assignmentDay, endDay, status, priority, type, owner } = form;
+
+    const ownerList: string[] = [];
+    owner.querySelectorAll('option:checked').forEach((item: any) => ownerList.push(item.value));
+    
+    const fieldsValues = {
+      title: title.value,
+      description: description.value,
+      assignmentDay: assignmentDay.value,
+      endDay: endDay.value,
+      status: status.value,
+      priority: priority.value,
+      type: type.value,
+      owner: ownerList,
     }
+    setFields(fieldsValues);
   }
 
   return (
     <div className="grid grid-cols-1 sm:px-8 sm:py-12 sm:gap-x-8 md:py-16">
-      <form onSubmit={create} onChange={onFieldChange} className="w-3/5 my-0 mx-auto">
+      <form name="actionItem" onSubmit={create} onChange={onFieldChange} className="w-3/5 my-0 mx-auto">
         {/* Título */}
         <div className="mb-6">
           <label htmlFor="title" className="block text-base text-gray-500">Título</label>
@@ -51,30 +63,34 @@ const CreateActionItem = () => {
           <textarea required id="description" className="border-gray-300 border w-full h-48 rounded-md p-2"></textarea>
         </div>
 
-        <div className="mb-6 flex justify-between">
+        <div className="mb-6 grid grid-cols-2 gap-5">
           {/* Data de início */}
           <div>
             <label htmlFor="assignmentDay" className="block text-base text-gray-500">Data de Início</label>
-            <input id="assignmentDay" type="date" className="border-gray-300 border w-96 rounded-md p-2" />
+            <input id="assignmentDay" type="date" className="border-gray-300 border w-full rounded-md p-2" />
           </div>
 
           {/* Data de conclusão */}
           <div>
             <label htmlFor="endDay" className="block text-base text-gray-500">Data de Conclusão</label>
-            <input id="endDay" type="date" className="border-gray-300 border w-96 rounded-md p-2" />
+            <input id="endDay" type="date" className="border-gray-300 border w-full rounded-md p-2" />
           </div>
         </div>
 
-        <div className="mb-6 flex justify-between">
-          <Select required label="Status" id="status" size="other" items={statusList} />
-          <Select required label="Prioridade" id="priority" size="other" items={priorityList} />
+        {/* Lista de Status e de Prioridades */}
+        <div className="mb-6 grid grid-cols-2 gap-5">
+          <Select required label="Status" id="status" size="full" items={statusList} />
+          <Select required label="Prioridade" id="priority" size="full" items={priorityList} />
         </div>
 
-        <div className="mb-6 flex justify-between">
-          <Select required label="Tipo" id="type" size="other" items={typeList} />
-          {fields && fields.type !== 'Boa Prática' && (
-            <Select required label="Responsável" id="owner" size="other" items={ownerList} />
-          )}
+        {/* Lista de tipos */}
+        <div className="mb-6">
+          <Select required label="Tipo" id="type" size="full" items={typeList} />
+        </div>
+
+        {/* Lista de responsáveis */}
+        <div className="mb-6">
+          <Select multiple label="Responsável" id="owner" size="full" items={ownerList} />
         </div>
         
         <div className="flex justify-end">
